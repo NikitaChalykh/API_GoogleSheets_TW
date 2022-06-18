e2b4b77adcbaa567d3cc76563dd1cd96347d1f73
Никита Чалых <Wizardus@list.ru>
2022-06-18 13:23:32 +0300

    add sheets utils

--- backend/polls/utils.py
+++ backend/polls/utils.py
@@ -1,10 +1,13 @@
 import datetime
 import os
 
+import apiclient.discovery
+import httplib2
 import requests
 import telegram
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
 
 load_dotenv()
 
@@ -22,7 +25,9 @@ def get_actual_dollar_currency():
 
 def send_telegram_message(message):
     '''Отправка уведомлений в Telegramm.'''
-    token = os.getenv('TELEGRAM_TOKEN')
+    token = os.getenv(
+        '2045985373:AAF9T9ZtOwCdA0kOzsDmdfgX6CKaskZylks'
+    )
     bot = telegram.Bot(token)
     chat_id = os.getenv('CHAT_ID')
     bot.send_message(chat_id, message)
@@ -36,3 +41,30 @@ def check_orders_delivery_date(orders):
         if order.delivery_date < today_date:
             lated_orders.append(order.order_number)
     return lated_orders
+
+
+def get_data_in_sheets():
+    '''Получаем данные из гугл талицы с заказами.'''
+    CREDENTIALS_FILE = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        'credentials.json'
+    )
+    ranges = ["Лист1!A:D"]
+    spreadsheetId = '1OpCylqw4U-64lMKZFVk7fhllqOvczTD5eb516aDghbo'
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        CREDENTIALS_FILE,
+        [
+            'https://www.googleapis.com/auth/spreadsheets',
+            'https://www.googleapis.com/auth/drive'
+        ]
+    )
+    httpAuth = credentials.authorize(httplib2.Http())
+    service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+    results = service.spreadsheets().values().batchGet(
+        spreadsheetId=spreadsheetId,
+        ranges=ranges,
+        valueRenderOption='FORMATTED_VALUE',
+        dateTimeRenderOption='FORMATTED_STRING'
+    ).execute()
+    sheet_values = tuple(results['valueRanges'][0]['values'])
+    return sheet_values
